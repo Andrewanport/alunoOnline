2662167e1e4c6b6834ac53577cdf7ec82952bc13
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2025-04-06 15:05:21 -0300

    Add files via upload

---
+++ api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -0,0 +1,38 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController                     // Preparando as rotas HTTP de acordo com a arquitetura REST
+@RequestMapping("/alunos")          // Mapeamento
+public class AlunoController {
+
+    // Injetando o service dentro do controller
+    @Autowired
+    AlunoService alunoService;
+
+    // POST = CRIAR
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarAluno(@RequestBody Aluno aluno){     // @RequestBody = Converte o objeto JSON enviado pelo FRONT para objeto JAVA
+
+        alunoService.criarAluno(aluno);
+
+    }
+
+    @DeleteMapping
+    public void excluirAluno(){
+
+    }
+
+    @PatchMapping
+    public void alterarParteAluno(){
+
+    }
+
+}
+
+// Postman e Insominia = Clientes HTTP para testar a aplicação backend
