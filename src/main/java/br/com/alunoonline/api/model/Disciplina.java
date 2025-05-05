b758ab58c98a3fb90198b4587ecf2f386ae47dfe
Andrewanport <andrewanderleyporto@gmail.com>
2025-05-05 16:26:11 -0300

    .

--- src/main/java/br/com/alunoonline/api/model/Disciplina.java
+++ src/main/java/br/com/alunoonline/api/model/Disciplina.java
@@ -14,11 +14,11 @@ public class Disciplina {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_disciplina")
+    @Column(name = "id")
     private Long id;
-    @Column(name = "nome_disciplina")
+    @Column(name = "nome")
     private String nome;
-    @Column(name = "carga_horaria_disciplina")
+    @Column(name = "carga_horaria")
     private Integer cargaHoraria;
     @ManyToOne
     @JoinColumn(name = "professor_id")
